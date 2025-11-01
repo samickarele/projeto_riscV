fbc8e669de797e8a49744d59b71617b75ac5f857
Samira Cikarele <samiracikareli@gmail.com>
2025-11-01 16:02:24 -0300

    primeiro teste

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -12,7 +12,8 @@ module ALUController (
 
   assign Operation[0] = ((ALUOp == 2'b10) && (Funct3 == 3'b110)) ||  // R\I-or
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) ||  // R\I->>
-      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000));  // R\I->>>
+      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
+      ((ALUop == 2'b10) && (Funct3 == 3'b100)); //R/I-XOR
 
   assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW
       ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||  // R\I-add
@@ -24,5 +25,7 @@ module ALUController (
       ((ALUOp == 2'b10) && (Funct3 == 3'b010));  // R\I-<
 
   assign Operation[3] = (ALUOp == 2'b01) ||  // BEQ
-      ((ALUOp == 2'b10) && (Funct3 == 3'b010));  // R\I-<
+      ((ALUOp == 2'b10) && (Funct3 == 3'b010)) || // R\I-<
+      ((ALUop == 2'b10) && (Funct3 == 3'b100)); //R/I XOR
+
 endmodule
--- design/alu.sv
+++ design/alu.sv
@@ -21,6 +21,10 @@ module alu#(
                     ALUResult = SrcA + SrcB;
             4'b1000:        // Equal
                     ALUResult = (SrcA == SrcB) ? 1 : 0;
+            4'b0001:       // or
+                    ALUResult = SrcA || SrcB;
+            4'b1001:      //xor
+                    ALUResult = SrcA ^ SrcB;
             default:
                     ALUResult = 0;
             endcase
