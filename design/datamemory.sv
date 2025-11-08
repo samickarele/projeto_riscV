1525f114a8eec2a06fdd83c4be5db2ddfc4673b0
litssaa <litssah@gmail.com>
2025-11-08 18:06:36 -0300

    slt e sh adicionados e n testados

--- design/datamemory.sv
+++ design/datamemory.sv
@@ -63,6 +63,14 @@ module datamemory #(
             default: Wr = 4'b0000;
           endcase
         end
+        3'b001: begin  // SH
+          Datain = {2{wd[15:0]}};
+          case (a[1])
+            1'b0: wr = 4'b0011;
+            1'b1: wr = 4'b1100;
+            default: wr = 4'b0000;
+          endcase  
+        end
         3'b010: begin  // SW
           Wr = 4'b1111;
           Datain = wd;
