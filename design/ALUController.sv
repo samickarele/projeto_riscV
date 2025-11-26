97496f38065201f6c99d9d13ff8f8be7a27175b0
Jéssica Macêdo <jessica.almeida.lopes.macedo@gmail.com>
2025-11-26 20:47:01 -0300

    bne, bge e blt implementados

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -14,16 +14,20 @@ module ALUController (
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) ||  // R\I->>
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) || // R\I->>>
       ((ALUOp == 2'b10) && (Funct3 == 3'b100))  || //R/I XOR 
-      ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000)); // sub
+      ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000)) || // sub
+      ((ALUOp == 2'b01) && (Funct3 == 3'b101));  // BGE
 
   assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW
       ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||  // R\I-add e sub
-      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)); // R\I->>>
+      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) || // R\I->>>
+      ((ALUOp == 2'b01) && (Funct3 == 3'b001));  // BNE
 
   assign Operation[2] =  ((ALUOp==2'b10) && (Funct3==3'b101) && (Funct7==7'b0000000)) || // R\I->>
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
       ((ALUOp == 2'b10) && (Funct3 == 3'b001)) ||  // R\I-<<
-      ((ALUOp == 2'b10) && (Funct3 == 3'b010));  // R\I-<
+      ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||  // R\I-<
+      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) ||  // BLT
+      ((ALUOp == 2'b01) && (Funct3 == 3'b101));    // BGE
 
   assign Operation[3] = (ALUOp == 2'b01) ||  // BEQ
       ((ALUOp == 2'b10) && (Funct3 == 3'b010)) || // R\I-<
--- design/alu.sv
+++ design/alu.sv
@@ -27,6 +27,12 @@ module alu#(
                     ALUResult = SrcA | SrcB;
             4'b1001: // XOR
                     ALUResult = SrcA ^ SrcB;
+            4'b1010: // BNE 
+                    ALUResult = (SrcA != SrcB) ? 1 : 0;
+            4'b1100: // BLT 
+                    ALUResult = ($signed(SrcA) < $signed(SrcB)) ? 1 : 0;
+            4'b1101: // BGE 
+                    ALUResult = ($signed(SrcA) >= $signed(SrcB)) ? 1 : 0;
             default:
                     ALUResult = 0;
             endcase
